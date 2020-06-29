edaca2bc7d16bdf0278553fd58a987d429b46345
Brad Beggs <brad.beggs@gmail.com>
2020-06-29 20:08:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,7 @@ def matrix_lookup(matrix, row, column)
   print matrix
   puts row
   puts column
-  matrix[row][column].sort! 
+  matrix[row][column] 
 end
 
 def matrix_update(matrix, row, column, new_value)
